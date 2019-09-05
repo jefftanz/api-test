4bfcdb38ef2bdee35f654e7f0320544245750971
jeffrey.tansey <>
2019-09-04 23:10:21 -0400

    updates 1

--- server.js
+++ server.js
@@ -41,6 +41,11 @@ router.get('/test', function(req, res) {
 	stepOne(RELEASE_ID);
 });
 
+router.get('/step3', function(req, res) {
+	res.json({ message: 'step 3 test' });
+	stepThree('cf44c070-b1b5-0137-8fb4-0a93ad5e9076');
+});
+
 // REGISTER OUR ROUTES -------------------------------
 app.use('/api', router);
 
@@ -55,10 +60,11 @@ var X_API_TOKEN = '1243fea9ebe3527f5ee7609c0ebee920953b10cb';
 var ORG_NAME = 'Jeff_Org1';
 var APP_NAME = 'app1';
 var RELEASE_ID = '3'; // Manually increment for testing?
-var FILE_LOCATION = '';
+var FILE_LOCATION = '/Users/jeffrey.tansey/repos/playground/ionic/ionic3/jeff_org1_app1_003.apk';
 
 // Step One of AppCenter Release process
-var stepOne = function(releaseId) {
+var stepOne = function() {
+	console.log('stepOne');
 
 	var headers = {
 		'Content-Type': 'application/json',
@@ -76,20 +82,29 @@ var stepOne = function(releaseId) {
 		if (!err && (httpResponse.statusCode == 200 || httpResponse.statusCode == 201)) {
 			console.log('statusCode: ' + httpResponse.statusCode);
 			console.log('body: ' + body);
-			stepTwo(body.upload_id, body.upload_url, releaseId);
+			
+			var myObject = JSON.parse(body);
+			console.log(myObject);
+			console.log('myObject.upload_url: ' + myObject.upload_url);
+
+			stepTwo(myObject.upload_id, myObject.upload_url);
 		} else {
 			console.log('error: ' + err);
+			console.log('statusCode: ' + httpResponse.statusCode);
 			// Send an error Message via Email
 		}
 	});
 }
 
 // Step Two of AppCenter Release process
-var stepTwo = function (uploadId, uploadUrl, releaseId) {
+var stepTwo = function (uploadId, uploadUrl) {
+	console.log('stepTwo');
 
 	var headers = {
 		'Content-Type': 'multipart/form-data',
 	}
+
+	console.log('uploadUrl: ' + uploadUrl);
 	
 	// TODO need File Source Location parameter
 	var options = {
@@ -102,12 +117,13 @@ var stepTwo = function (uploadId, uploadUrl, releaseId) {
 	}
 
 	request.post(options, function (err, httpResponse, body) {
-		if (!err && (httpResponse.statusCode == 200 || httpResponse.statusCode == 201)) {
+		if (!err && httpResponse.statusCode == 204) {
 			console.log('statusCode: ' + httpResponse.statusCode);
 			console.log('body: ' + body);
-			stepThree(uploadId, releaseId);
+			stepThree(uploadId);
 		} else {
 			console.log('error: ' + err);
+			console.log('statusCode: ' + httpResponse.statusCode);
 			// Send an error Message via Email
 		}
 	});
@@ -115,7 +131,9 @@ var stepTwo = function (uploadId, uploadUrl, releaseId) {
 }
 
 // Step Three of AppCenter Release process
-var stepThree = function (uploadId, releaseId) {
+var stepThree = function (uploadId) {
+	console.log('stepThree');
+	console.log('uploadId: ' + uploadId);
 
 	var headers = {
 		'Content-Type': 'application/json',
@@ -128,19 +146,29 @@ var stepThree = function (uploadId, releaseId) {
 		url: "https://api.appcenter.ms/v0.1/apps/"+ORG_NAME+"/"+APP_NAME+"/release_uploads/"+uploadId,
 		headers: headers,
 		rejectUnauthorized: false,
-		formData: {
-			status: 'comitted'
+		json: true,
+		body: {
+			"status": "committed"
 		}
 	}
 
-	request.patch(options, function (err, httpResponse, body) {
-		if (!err && (httpResponse.statusCode == 200 || httpResponse.statusCode == 201)) {
+	request.patch(options, function (err, httpResponse, bodyResponse) {
+		console.log('after request.patch');
+
+		if (!err && httpResponse.statusCode == 200) {
 			console.log('statusCode: ' + httpResponse.statusCode);
-			console.log('body: ' + body);
-			stepFour(releaseId);
+
+			console.log('bodyResponse: ' + bodyResponse);
+			console.log('bodyResponse.release_id: ' + bodyResponse.release_id);
+			// var myObject2 = JSON.stringify(bodyResponse);
+			// var myObject2 = JSON.parse(bodyResponse);
+			// console.log('myObject: ' + myObject2);
+			// console.log('myObject.release_id: ' + myObject2.release_id);
+
+			stepFour(bodyResponse.release_id);
 		} else {
 			console.log('error: ' + err);
-			// Fail the upload process
+			console.log('statusCode: ' + httpResponse.statusCode);
 			// Send an error Message via Email
 		}
 	});
@@ -149,6 +177,7 @@ var stepThree = function (uploadId, releaseId) {
 
 // Step Four of AppCenter Release process
 var stepFour = function (releaseId) {
+	console.log('stepFour');
 
 	var headers = {
 		'Content-Type': 'application/json',
@@ -160,20 +189,23 @@ var stepFour = function (releaseId) {
 		url: "https://api.appcenter.ms/v0.1/apps/"+ORG_NAME+"/"+APP_NAME+"/releases/"+releaseId,
 		headers: headers,
 		rejectUnauthorized: false,
-		formData: {
-			destination_name: 'Collaborators'
+		json: true,
+		body: {
+			"destination_name": "Collaborators", 
 		}
 		//TODO check to see if Collaborators is default group? Need to check the example with 100's of apps from transitioning 
 		//	from HockeyApp to AppCenter
 	}
 
 	request.patch(options, function (err, httpResponse, body) {
-		if (!err && (httpResponse.statusCode == 200 || httpResponse.statusCode == 201)) {
+		console.log('after request.patch');
+
+		if (!err && httpResponse.statusCode == 200) {
 			console.log('statusCode: ' + httpResponse.statusCode);
 			console.log('body: ' + body);
 		} else {
 			console.log('error: ' + err);
-			// Fail the upload process
+			console.log('statusCode: ' + httpResponse.statusCode);
 			// Send an error Message via Email
 		}
 	});
