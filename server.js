87f075b069631f6001be1ca8e8f13381d021ab22
jeffrey.tansey <>
2019-10-15 15:46:32 -0400

    test123

--- server.js
+++ server.js
@@ -36,13 +36,215 @@ router.get('/', function(req, res) {
 	res.json({ message: 'hooray! welcome to our api!' });	
 });
 
-var params = {
-	'xApiToken': '1243fea9ebe3527f5ee7609c0ebee920953b10cb',
-	'orgName': 'Jeff_Org1',
-	'appName': 'app3',
-	'displayName': 'app3',
-	'os': 'Android',
-	'src': '/Users/jeffrey.tansey/repos/playground/ionic/ionic3/jeff_org1_app1_004.apk'
+var params2 = {
+	xApiToken: '[TOKEN_HERE]',
+	orgName: '[ORG_NAME_HERE]'
+}
+
+// Test getting all OrgApps
+router.get('/update-apps', function(req, res) {
+	getOrgAppsV2();
+});
+
+// Get all the Apps from the Organization
+// Using display_name
+var getOrgAppsV2 = function () {
+
+	console.log('getOrgAppsV2');
+
+	var headers = {
+		'Content-Type': 'application/json',
+		'Accept': 'application/json',
+		'X-API-Token': params2.xApiToken
+	}
+	
+	var options = {
+		url: "https://api.appcenter.ms/v0.1/orgs/"+params2.orgName+"/apps",
+		headers: headers,
+		rejectUnauthorized: false
+	}
+
+	request.get(options, function (err, httpResponse, body) {
+
+		console.log('getOrgApps statusCode: ' + httpResponse.statusCode);
+		
+		if (!err && httpResponse.statusCode == 200) {
+
+			var myApps = JSON.parse(body);
+
+			console.log('apps: ' + myApps.length);
+			for (var i = 0; i < myApps.length; i++){
+
+				var appName = myApps[i].name;
+				var appNamePK = myApps[i].display_name;
+
+				var lowerTest = myApps[i].display_name.toLowerCase();
+
+				if (lowerTest.includes("lscu") ||
+					lowerTest.includes("forward") || 
+						lowerTest.includes("nuvision") || 
+						lowerTest.includes("ps") || 
+						lowerTest.includes("cardguard") || 
+						lowerTest.includes("freedom") || 
+						lowerTest.includes("community") ||
+						lowerTest.includes("bankfund")
+				){
+
+					appNamePK = appNamePK.replace(/[^a-zA-Z0-9]/g, " ");
+					appNamePK = appNamePK.trim();
+					appNamePK = appNamePK.replace(/\s+/g, "-");
+	 
+					appNamePK = appNamePK + "-" + myApps[i].os;
+	
+					console.log('old: ' +appName+ ", new: " +appNamePK);
+	
+					updateAppNameV1(appName, appNamePK);
+
+				}
+
+			}
+		} else {
+			if (err == undefined || err == null){
+				var errObj = JSON.parse(body);
+				console.log('error: ' + errObj.code + ' ' + errObj.message); 
+			} else {
+				console.log('error: ' + err);
+			}
+		}
+	});
+}
+
+// Using the 'name' field 
+var updateAppNameV1 = function (appName, newAppName) {
+
+	// TODO CHANGE PER APP NAME 
+	// if (!appName.toLowerCase().includes('mun')){
+	// 	return;
+	// } else {
+	// 	console.log('*** appName contains harvard: ' + appName + ' ***');
+	// }
+	
+	var headers = {
+		'Content-Type': 'application/json',
+		'Accept': 'application/json',
+		'X-API-Token': params2.xApiToken
+	}
+
+	var options = {
+		url: "https://api.appcenter.ms/v0.1/apps/"+params2.orgName+"/"+appName,
+		headers: headers,
+		rejectUnauthorized: false,
+		json: true,
+		body: {
+			"name": newAppName
+		}
+	}
+
+	request.patch(options, function (err, httpResponse, body) {
+
+		//console.log('updateAppName result: ' + httpResponse.statusCode);
+		
+		if (!err && httpResponse.statusCode == 200) {
+			console.log('Updated name from ' + appName + ' to ' + newAppName);
+		} else {
+			console.log('Failed name from ' + appName + ' to ' + newAppName);
+			if (err == undefined || err == null){
+				console.log('error: ' + httpResponse.body.statusCode + ' ' + httpResponse.body.message); 
+			} else {
+				console.log('error: ' + err);
+			}
+		}
+	});
+}
+
+
+// Test getting all OrgApps
+router.get('/test-name', function(req, res) {
+	
+	var appNamePK = "Test – Dash";
+	console.log('original: ' + appNamePK);
+	appNamePK = appNamePK.replace(/[^a-zA-Z0-9]/g, " ");
+	console.log('spaces: ' + appNamePK);
+	appNamePK = appNamePK.trim();
+	console.log('trim: '+appNamePK);
+	appNamePK = appNamePK.replace(/\s+/g, "-");
+	console.log('Add Dashes: '+appNamePK);
+	appNamePK = appNamePK + "-Android";
+	console.log('Android: '+appNamePK);
+
+
+});
+
+
+
+
+
+// Get all the Apps from the Organization
+var getOrgAppsV1 = function () {
+
+	console.log('getOrgApps');
+
+	var params = {
+		xApiToken: '1243fea9ebe3527f5ee7609c0ebee920953b10cb'
+	}
+
+	var headers = {
+		'Content-Type': 'application/json',
+		'Accept': 'application/json',
+		'X-API-Token': params.xApiToken
+	}
+	
+	var options = {
+		url: "https://api.appcenter.ms/v0.1/orgs/Jeff_Org1/apps",
+		headers: headers,
+		rejectUnauthorized: false
+	}
+
+	request.get(options, function (err, httpResponse, body) {
+
+		console.log('getOrgApps statusCode: ' + httpResponse.statusCode);
+		
+		if (!err && httpResponse.statusCode == 200) {
+
+			var myApps = JSON.parse(body);
+
+			console.log('apps: ' + myApps.length);
+			for (var i = 0; i < myApps.length; i++){
+
+				// Looking for -#'s at the end of app names.
+
+				var appName = myApps[i].name;
+				var appOS = myApps[i].os;
+				var lastLetter = appName.charAt(appName.length - 1);
+				var secondLastLetter = appName.charAt(appName.length - 2);
+				var isNumber = !isNaN(lastLetter);
+				var isDash = (secondLastLetter == '-');
+
+				// var res = str.substr(1, 4);
+
+				var newAppName = '';
+
+				if (isNumber && isDash){
+					console.log('isNumber and has dash: ' + appName);
+				 newAppName = appName.substr(0, appName.length - 2);
+					newAppName = newAppName + '-' + appOS;
+				} else {
+					console.log('normal name: ' + appName);
+					newAppName = appName + '-' + appOS;
+				}
+
+				updateAppNameV1(appName, newAppName);
+
+			}
+		} else {
+			if (err == undefined || err == null){
+				var errObj = JSON.parse(body);
+				console.log('error: ' + errObj.code + ' ' + errObj.message); 
+			} else {
+				console.log('error: ' + err);
+			}
+		}
+	});
 }
 
 router.get('/upload', function(req, res) {
